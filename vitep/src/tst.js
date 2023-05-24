9819cacb16f544030a76b16454af39e2ee7cc715
unknown <drOneMB.marcinbernas@gmail.com>
2023-05-24 08:59:52 +0200

    F commit

---
+++ vitep/src/tst.js
@@ -0,0 +1,13 @@
+<Route
+  action={async ({ params, request }) => {
+    const res = await fetch(
+      `/api/properties/${params.id}`,
+      {
+        method: "put",
+        body: await request.formData(),
+      }
+    );
+    if (!res.ok) throw res;
+    return { ok: true };
+  }}
+/>
